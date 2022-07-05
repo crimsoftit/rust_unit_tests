38d623a2d77756d9b8b974f5e8dbad7de54e82bb
crimsoftit <crimsoftit@gmail.com>
2022-07-05 22:02:24 +0300

    1. rust unit tests

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,8 @@ pub fn main() {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn test_assert () {
         assert!(5 > 4);
@@ -25,10 +27,17 @@ mod tests {
     #[test]
     fn test_assert_eq () {
         assert_eq!(4, 4);
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_addition () {
+        panik::test_panik(40);
     }
 
     #[test]
     fn test_assert_ne () {
-        assert_ne!(3, 2);
+        assert_ne!(3, 2, "correct!!");
     }
 }
